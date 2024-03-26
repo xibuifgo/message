0d81bd0317777804fa2f55ea4cf48b3eeff8763a
xibuifgo <xibuifgo@gmail.com>
2024-03-26 17:46:43 +0300

    Added timer

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 18th!";
+  question.innerHTML = "OH YAAAAAAAAAAA! <br> I know you're upset but trust me you've got this";
   gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+    "https://media.giphy.com/media/SkAkim7JbUG01dGzQ0/giphy.gif";
 });
 
 noBtn.addEventListener("mouseover", () => {
@@ -20,4 +20,21 @@ noBtn.addEventListener("mouseover", () => {
 
   noBtn.style.left = randomX + "px";
   noBtn.style.top = randomY + "px";
+
+  original_src = "https://media.giphy.com/media/cbOun7SXza6TpmyUTZ/giphy.gif";
+  original_text = question.innerHTML;
+
+  gif.src = 
+  "https://media.giphy.com/media/Yt6GOxCQYXXevSZu4U/giphy.gif";
+
+  question.innerHTML = "WHATTTTTTTTTTTTTTTT";
+
+  setTimeout(function() {
+    question.innerHTML = "Try again";
+  }, 2000);
+
+  setTimeout(function() {
+    gif.src = original_src;
+    question.innerHTML = original_text;
+  }, 4000);
 });
